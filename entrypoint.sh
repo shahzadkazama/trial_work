b2f77b4c22797e6e921ad5f16338858d8d140aff
shahzadkazama <shahzad_kazama@hotmail.com>
2021-05-16 14:32:14 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
 cd /code/Swyft
-mkdir /lfls_data
-mkdir /lfls_data/uploads
-mkdir /lfls_data/exporter
-mkdir /lfls_data/log_files
-chmod 777 -R /lfls_data
-
-#mkdir /code/dataprep_api/logs
-
-#echo "Starting Celery"
-#celery -A dataprep_api worker --loglevel=debug --logfile=celery.logs &
 
 echo "Making migrations"
-###python manage.py makemigrations
+python manage.py makemigrations
 
 # Apply database migrations
 echo "Apply database migrations"
@@ -27,11 +17,6 @@ rm /etc/nginx/sites-enabled/default
 
 echo "Starting nginx"
 /usr/sbin/nginx
-
-echo "starting cron"
-service cron start
-
-cp /code/Swyft/web_app/order_complition_mail.py /code/Swyft/
 
 gunicorn --timeout 1800 --access-logfile - --workers 3 --bind unix:/code/Swyft/Swyft.sock Swyft.wsgi:application
 
